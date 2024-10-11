be6b81e271a82870791ec478dc2ec800f0a3cab9
Cesca <32521038+cescaa@users.noreply.github.com>
2024-10-11 16:44:33 -0400

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,63 @@
+import { useState, useEffect } from "react";
+import useSWR from "swr";
+import { Accordion, Pagination } from "react-bootstrap";
+import ListingDetails from "@/components/ListingDetails";
+import PageHeader from "@/components/PageHeader";
+
+export default function Home() {
+  // add states
+  const [page, setPage] = useState(1);
+  const [pageData, setPageData] = useState([]);
+
+  // request data
+  //const { data, error } = useSWR(`http://localhost:8000/api/listings/?page=${page}&perPage=10`);
+  const { data, error } = useSWR(
+    `https://web422-a1-nine.vercel.app/api/listings/?page=${page}&perPage=10`
+  ); // NOTE: DOES NOT WORK; DATA SHOWS LOCALLY ONLY
+
+  useEffect(() => {
+    if (data) {
+      setPageData(data);
+    }
+  }, [data]);
+
+  // set page state
+  const prev = () => {
+    if (page > 1) {
+      setPage((p) => p - 1);
+    }
+  };
+  const next = () => {
+    setPage((p) => p + 1);
+  };
+
+  // https://react-bootstrap.github.io/docs/components/accordion/
+  return (
+    <>
+      <PageHeader prop="Browse Listings: Sorted by Number of Ratings" />
+      <Accordion>
+        {pageData.map((listing) => (
+          <Accordion.Item key={listing._id} eventKey={listing._id}>
+            <Accordion.Header>
+              <p>
+                <strong>{listing.name}</strong>
+                <br />
+                <br />
+                {listing.address.street}
+              </p>
+            </Accordion.Header>
+            <Accordion.Body>
+              <ListingDetails listing={listing} />
+            </Accordion.Body>
+          </Accordion.Item>
+        ))}
+      </Accordion>
+
+      <Pagination>
+        <Pagination.Prev onClick={prev} disabled={page === 1} />
+        <Pagination.Item>{page}</Pagination.Item>
+        <Pagination.Next onClick={next} />
+      </Pagination>
+    </>
+  );
+}
